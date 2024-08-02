c0e3b8386b2e4d7d3c729ad6e65c1a1aca4ac122
Aayush <aayushshetty03@gmail.com>
2024-08-02 19:03:11 +0530

    Update server.js

--- Task3/server.js
+++ Task3/server.js
@@ -17,7 +17,7 @@ function getHouse() {
   return houses[Math.floor(Math.random() * houses.length)];
 }
 
-mongoose.connect(url,).then(() => {
+mongoose.connect(url).then(() => {
   console.log('Connected to MongoDB');
 }).catch(e => {
   console.error('Error connecting to MongoDB:', e);
